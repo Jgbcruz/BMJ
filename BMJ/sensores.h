e36f00acd1f18a4be705b3b98707332ced5f89af
Jgbcruz <162941859+Jgbcruz@users.noreply.github.com>
2026-07-14 00:40:17 -0300

    Modify sensor pin definitions in sensores.h
    Updated sensor pin definitions and removed unused sensor variables.

--- BMJ/sensores.h
+++ BMJ/sensores.h
@@ -4,19 +4,18 @@
 //BMJ
 // ultima atualização 16/05/2026
 #define FEsq 12
-#define Frente 7
-#define FDir 8
-#define linhaEsq XX
-#define linhaDir XX
+#define Frente 26
+#define FDir 14
+// #define linhaEsq XX
+// #define linhaDir XX
 
-uint8_t sensores[] = {FEsq, Frente, FDir, linhaEsq, linhaDir};
+
+uint8_t sensores[] = {FEsq, Frente, FDir};
 
 void setupSensores() {
   pinMode(FEsq, INPUT);
   pinMode(Frente, INPUT);
   pinMode(FDir, INPUT);
-  pinMode(linhaEsq, INPUT);
-  pinMode(linhaDir, INPUT);
 }
 
 #endif
